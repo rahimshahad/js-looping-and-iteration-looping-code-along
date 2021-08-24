5a6d1d372e39d002d8d67a0d79b93f3acd2eefac
rahimshahad <rahim.shahad2@gmail.com>
2021-08-24 01:32:57 -0400

    commit tree 95e940d2020abccb0c0cbbba1b59fcc52b59b912

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(people, gift) {
+    let message = []
+    for (let i = 0; i < people.length; i++) {
+        message[i] = (`Thank you, ${people[i]}, for the wonderful ${gift} gift!`);
+    }
+    return message;
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
